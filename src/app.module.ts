07cd03303944be0caaf6becc994002175dcd5af3
josephaven <jr457944@gmail.com>
2025-03-20 00:57:28 -0600

    Configuración de JWT

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module'; 
+import { User } from './users/user.entity';
 
 @Module({
-  controllers: [AppController], // Asegúrate de que el controlador esté aquí
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433, 
+      username: 'postgres',
+      password: 'N16j1204',
+      database: 'viayage_db',
+      entities: [User],
+      synchronize: true,
+    }),
+    AuthModule, 
+  ],
 })
 export class AppModule {}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../users/user.entity';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private jwtService: JwtService,
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  async register(email: string, password: string) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = this.userRepository.create({ email, password: hashedPassword });
+    await this.userRepository.save(user);
+    return { message: 'Usuario registrado exitosamente' };
+  }
+
+  async login(email: string, password: string) {
+    const user = await this.userRepository.findOne({ where: { email } });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      throw new UnauthorizedException('Credenciales incorrectas');
+    }
+    const token = this.jwtService.sign({ userId: user.id });
+    return { access_token: token };
+  }
+}
